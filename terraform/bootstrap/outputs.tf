b42924c5d931cd4c0adea4c2944dd677cb12af81
Kang-minjong <kjskkdkmj@gmail.com>
2026-03-30 16:28:49 +0900

    Modified outputs.tf
    I modified outputs terraform file

--- terraform/bootstrap/outputs.tf
+++ terraform/bootstrap/outputs.tf
@@ -4,17 +4,17 @@
 
 output "terraform_state_bucket_name" {
   description = "상태 S3 버킷 이름"
-  value       = aws_s3_bucket.terraform_state.bucket
+  value       = aws_s3_bucket.state_bucket.bucket
 }
 
 output "terraform_lock_table_name" {
   description = "DynamoDB Lock 테이블이름"
-  value       = aws_dynamodb_table.terraform_lock.name
+  value       = aws_dynamodb_table.terraform_lock_table.name
 }
 
 output "github_actions_role_arn" {
   description = "Github Actions을 위한 IAM Role ARN"
-  value       = aws_iam_role.github_actions.arn
+  value       = aws_iam_role.github_actions_iam_role.arn
 }
 
 output "aws_account_id" {
