e8972d2381f8c5d8f28d1ee477a5e93f7c8ba33f
Kang-minjong <kjskkdkmj@gmail.com>
2026-03-30 16:09:55 +0900

    Change Local Variables  and Description
    I changed Local Variables and Description.

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -36,7 +36,7 @@ resource "aws_s3_bucket_public_access_block" "state_access_block" {
 }
 
 # 락을 위한 DynamoDB 테이블 생성
-resource "aws_dynamodb_table" "terraform_lock" {
+resource "aws_dynamodb_table" "terraform_lock_table" {
   name         = var.terraform_lock_table_name
   billing_mode = "PAY_PER_REQUEST" # On-Demand 청구
   hash_key     = "LockID"
@@ -48,7 +48,7 @@ resource "aws_dynamodb_table" "terraform_lock" {
 }
 
 # GitHub Actions를 위한 OIDC 인증 (변경금지)
-resource "aws_iam_openid_connect_provider" "github" {
+resource "aws_iam_openid_connect_provider" "github_oidc_provider" {
   url = "https://token.actions.githubusercontent.com" # OIDC 공식주소
 
   client_id_list = [
@@ -60,25 +60,25 @@ resource "aws_iam_openid_connect_provider" "github" {
   ]
 }
 
-# IAM trust policy for GitHub Actions
-data "aws_iam_policy_document" "github_actions_assume_role" {
+# GitHub Actions의 OIDC토큰의 IAM 역할 정의
+data "aws_iam_policy_document" "github_actions_assume_role_policy" {
   statement {
     effect = "Allow"
 
     actions = [
-      "sts:AssumeRoleWithWebIdentity"
+      "sts:AssumeRoleWithWebIdentity" # OI
     ]
 
     principals {
-      type        = "Federated"
-      identifiers = [aws_iam_openid_connect_provider.github.arn]
+      type        = "Federated" # 외부 인증 (OIDC)
+      identifiers = [aws_iam_openid_connect_provider.github_oidc_provider.arn]
     }
 
     condition {
       test     = "StringEquals"
       variable = "token.actions.githubusercontent.com:aud"
       values   = ["sts.amazonaws.com"]
-    }
+    } # 조건 1 : AWS STS 용으로만 발급된 토큰만 허용
 
     condition {
       test     = "StringLike"
@@ -86,17 +86,18 @@ data "aws_iam_policy_document" "github_actions_assume_role" {
       values = [
         "repo:${var.github_org}/${var.github_repo}:ref:refs/heads/${var.github_branch}"
       ]
-    }
+    } # 조건 2 : 특정 레포지토리, 경로, 브랜치에서 온 토큰만 허용
   }
 }
 
-resource "aws_iam_role" "github_actions" {
+# IAM 정책을 연결할
+resource "aws_iam_role" "github_actions_iam_role" {
   name               = var.github_actions_role_name
-  assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
+  assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role_policy.json
 }
 
 # Github Actions을 Terraform에서 사용하기 위한 IAM 정책
-data "aws_iam_policy_document" "github_actions_terraform" {
+data "aws_iam_policy_document" "github_actions_permissions_policy_document" {
   statement {
     sid    = "AllowStateBucketAccess" # 임의의 상태 ID
     effect = "Allow"
@@ -138,7 +139,7 @@ data "aws_iam_policy_document" "github_actions_terraform" {
     ]
 
     resources = [
-      aws_dynamodb_table.terraform_lock.arn
+      aws_dynamodb_table.terraform_lock_table.arn
     ]
   }
 
@@ -167,14 +168,14 @@ data "aws_iam_policy_document" "github_actions_terraform" {
   }
 }
 
-# aws에 github actions을 위한 실제 정책 리스트를 생성
-resource "aws_iam_policy" "github_actions_terraform" {
+# aws에 github actions을 위한 실제 IAM 정책을 생성
+resource "aws_iam_policy" "github_actions_permissions_policy" {
   name   = "${var.project_name}-${var.environment}-github-actions-terraform-policy"
-  policy = data.aws_iam_policy_document.github_actions_terraform.json
+  policy = data.aws_iam_policy_document.github_actions_permissions_policy_document.json # 위에서 만든 IAM 정책을 json 형태로 변환
 }
 
-# 생성한 정책 리스트에 만든 IAM 정책들을 연결
-resource "aws_iam_role_policy_attachment" "github_actions_terraform" {
-  role       = aws_iam_role.github_actions.name
-  policy_arn = aws_iam_policy.github_actions_terraform.arn
+# 만든 IAM 정책들을 ROLE에 연결
+resource "aws_iam_role_policy_attachment" "github_actions_permissions_attachment" {
+  role       = aws_iam_role.github_actions_iam_role.name
+  policy_arn = aws_iam_policy.github_actions_permissions_policy.arn # 방금 생성한 IAM 정책의 주소
 }
